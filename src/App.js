abf46b45b71b2f2d5baccee61167d78e819cef18
Justin Dennison <justin1dennison@gmail.com>
2018-02-07 18:42:58 -0500

    implemented the incrementing functionality for each of the buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    }
+
+    this.increment = this.increment.bind(this);
+  }
+  increment(value){
+    this.setState({count: this.state.count + value});
+  }
   render() {
     return (
       <div>
-        
+        <h1>{this.state.count}</h1>
+        <button onClick={() => this.increment(1)}>1</button>
+        <button onClick={() => this.increment(2)}>2</button>
+        <button onClick={() => this.increment(3)}>3</button>
       </div>
     );
   }
