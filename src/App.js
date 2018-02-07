2bae80b09e5a6c56391fd2c0d52573bbfb79125c
Justin Dennison <justin1dennison@gmail.com>
2018-02-07 18:44:55 -0500

    implemented the reset button functionality

--- src/App.js
+++ src/App.js
@@ -8,17 +8,26 @@ class App extends Component {
     }
 
     this.increment = this.increment.bind(this);
+    this.reset = this.reset.bind(this);
   }
   increment(value){
     this.setState({count: this.state.count + value});
+  }
+  reset(){
+    this.setState({count: 0});
   }
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
-        <button onClick={() => this.increment(1)}>1</button>
-        <button onClick={() => this.increment(2)}>2</button>
-        <button onClick={() => this.increment(3)}>3</button>
+        <div>
+          <button onClick={() => this.increment(1)}>1</button>
+          <button onClick={() => this.increment(2)}>2</button>
+          <button onClick={() => this.increment(3)}>3</button>
+        </div>
+        <div>
+          <button onClick={this.reset}>Reset</button>
+        </div>
       </div>
     );
   }
